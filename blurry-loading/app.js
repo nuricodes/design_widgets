4b2ea3fe3347dcaa091a909d8a54d871d9b152f5
ayninur <nuralaynabdulqadir@gmail.com>
2020-12-12 17:12:55 -0500

    incrementing integers displaying on screen

---
+++ blurry-loading/app.js
@@ -0,0 +1,19 @@
+// SELECT ELEMENTS
+const loadText = document.querySelector('.loading-text')
+const bg = document.querySelector('.bg')
+
+let load = 0;
+
+let int = setInterval(blurring, 30)
+
+function blurring() {
+    load++
+
+    if (load > 99) {
+        clearInterval(int)
+    }
+    loadText.innerHTML = `${load}%`
+    loadText.style.opacity = 
+}
+
+// (4:21)
