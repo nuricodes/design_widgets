8431ac29721b07b5a3c105cb376d864f3894dfa7
ayninur <nuralaynabdulqadir@gmail.com>
2021-02-13 22:35:25 -0500

    Sound player (HTML5 audio tag added comments

--- sound-board/app.js
+++ sound-board/app.js
@@ -1,16 +1,23 @@
 const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
 
 sounds.forEach(sound => {
+    // Create button element
     const btn = document.createElement('button')
+    //add a class
     btn.classList.add('btn')
 
+    //insert text 
     btn.innerText = sound
 
+    //make button element functional
     btn.addEventListener('click', () => {
+        //do this with function below
         stopSongs()
+        //when clicked play audio tag in html
         document.getElementById(sound).play()
     })
 
+    //append newly created button to btn div
     document.getElementById('buttons').appendChild(btn)
 })
 
