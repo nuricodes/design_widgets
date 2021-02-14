3e7cda099990dc2e8d107d3738f8a90dd501695c
ayninur <nuralaynabdulqadir@gmail.com>
2021-02-14 00:20:06 -0500

    request using async await

--- dad-jokes/app.js
+++ dad-jokes/app.js
@@ -5,8 +5,25 @@ const jokeBtn = document.getElementById('jokeBtn');
 jokeBtn.addEventListener('click', generateJoke)
 generateJoke()
 
-//call from api
-function generateJoke() {
+// //call from api using fetch
+// function generateJoke() {
+//     //below (accept) ensures we get json back not html
+//     const config = {
+//         headers: {
+//             'Accept': 'application/json'
+//         }
+//     }
+
+//     fetch('https://icanhazdadjoke.com/', config)
+//         .then(res => res.json())
+//         .then(data => {
+//             jokeEl.innerHTML = data.joke
+
+//         })
+// }
+
+//call from api using async await
+async function generateJoke() {
     //below (accept) ensures we get json back not html
     const config = {
         headers: {
@@ -14,10 +31,11 @@ function generateJoke() {
         }
     }
 
-    fetch('https://icanhazdadjoke.com/', config)
-        .then(res => res.json())
-        .then(data => {
-            jokeEl.innerHTML = data.joke
+    const res = await fetch('https://icanhazdadjoke.com/', config)
 
-        })
+    const data = await res.json()
+    jokeEl.innerHTML = data.joke
+
+
+
 }
